54fca86e2e45a67dc8dee802079a4f1788692bac
Aaron DeGrow <adegrow@ford.com>
2013-10-07 09:15:01 -0400

    Added comments for common.py.

--- common.py
+++ common.py
@@ -1,5 +1,20 @@
-# General direction enum
 class Direction:
+    """Enumerates movement directions
+
+    This enumeration is used to describe the direction of an class that
+    provides physical movement (e.g., an Arm or Drive Train).  All directions
+    are from the perspective of the center of the robot facing towards the
+    front.
+
+    Attributes:
+        left.
+        right.
+        forward.
+        backward.
+        up.
+        down.
+
+    """
     left = 1
     right = 2
     forward = 3
@@ -7,8 +22,18 @@ class Direction:
     up = 5
     down = 6
 
-# Robot state enum
 class ProgramState:
+    """Enumerates robot game states.
+
+    This enumeration is used to keep track of the current game state as
+    provided by the playing field (FMS).
+
+    Attributes:
+        disabled.
+        autonomous.
+        teleop.
+
+    """
     disabled = 1
     autonomous = 2
     teleop = 3
